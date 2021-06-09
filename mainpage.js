d64d110813e50717a80a7550bf09e8a37c0af39d
Ruslan Tikhov <56027788+Ruslansergeevich96@users.noreply.github.com>
2021-06-09 20:48:00 +0300

    Add files via upload

--- mainpage.js
+++ mainpage.js
@@ -1,3 +1,22 @@
 $(document).ready(function() {
     $("header").toggleClass("header-glass");
-  });
+});
+
+/* Добавление нового портфеля*/
+$("#createportfolio").click(function () {
+    $(".table_col > tbody:last").append('<tr> <td>Первый портфель</td> <td>151212</td> <td>0.12323223</td> <td>+1232500$</td> <td>12.12.2012</td> <td><input type="button" class="delete" name="delete" value="Удалить" ></td> </tr>');    
+});
+/* Удаление портфелея*/    
+                
+            
+
+
+$('.delete').click(function(){
+    $(this).parent().parent().remove();
+        return false;
+});
+
+/*  Donut Chart */
+
+
+
